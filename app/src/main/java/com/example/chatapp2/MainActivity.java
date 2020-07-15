770de84443dbd9703a7b20b54620dfb3cd6a73a9
Juyoung <juyoung2677@gmail.com>
2020-07-15 15:27:21 +0900

    7. PeopleFragment

--- app/src/main/java/com/example/chatapp2/MainActivity.java
+++ app/src/main/java/com/example/chatapp2/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.chatapp2.fragment.PeopleFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainActivity_frameLayout, new PeopleFragment()).commit();
     }
 }
---
+++ app/src/main/java/com/example/chatapp2/fragment/PeopleFragment.java
@@ -0,0 +1,100 @@
+package com.example.chatapp2.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.chatapp2.R;
+import com.example.chatapp2.model.UserModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PeopleFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_people,container,false);
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.peopleFragment_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(inflater.getContext()));
+        recyclerView.setAdapter(new PeopleFragmentRecyclerViewAdapter());
+
+        return view;
+    }
+
+    class PeopleFragmentRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+        List<UserModel> userModels;
+        public PeopleFragmentRecyclerViewAdapter() {
+            userModels = new ArrayList<>();
+            FirebaseDatabase.getInstance().getReference().child("users").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    userModels.clear(); // 친구 목록 쌓일 때 중복되는 것 제거
+                    // 서버에서 넘어온 친구 목록이 담기는 곳
+                    for(DataSnapshot snapshot :dataSnapshot.getChildren()){
+                        userModels.add(snapshot.getValue(UserModel.class));
+                    }
+                    notifyDataSetChanged(); // 데이터 쌓이고 새로고침 해서 불러올 수 있게 해주는 것!
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_friend,parent,false);
+            return new CustomViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            // 이미지 넣어줄 곳
+            Glide.with
+                    (holder.itemView.getContext())
+                    .load(userModels.get(position).profileImageUrl)
+                    .apply(new RequestOptions().circleCrop())
+                    .into(((CustomViewHolder)holder).imageView);
+
+            ((CustomViewHolder)holder).textView.setText(userModels.get(position).userName);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return userModels.size();
+        }
+
+        private class CustomViewHolder extends RecyclerView.ViewHolder {
+            public ImageView imageView;
+            public TextView textView;
+
+            public CustomViewHolder(View view) {
+                super(view);
+                imageView = view.findViewById(R.id.friendItem_imageView);
+                textView = (TextView)view.findViewById(R.id.friendItem_textView);
+            }
+        }
+    }
+}
